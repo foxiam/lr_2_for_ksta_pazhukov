313e57a3f9f477e2206df7a7d302231850b3984d
foxiam <main.4xiam@gmail.com>
2023-11-10 01:35:47 +0300

    rebuild for only second values

--- main.cpp
+++ main.cpp
@@ -1,19 +1,21 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-   int N;
-   cout << "Введите количество итераций N: ";
-   cin >> N;
+int main()
+{
+    int N;
+    cout << "Введите количество итераций N: ";
+    cin >> N;
 
-   int a = 0, b = 1, nextTerm;
-   cout << "Ряд чисел Фибоначчи до " << N << " итераций: ";
+    int a = 0, b = 1, nextTerm;
+    cout << "Ряд чисел Фибоначчи до " << N << " итераций: ";
 
-   for (int i = 1; i <= N; ++i) {
-      cout << a << " ";
-      nextTerm = a + b;
-      a = b;
-      b = nextTerm;
-   }
-   return 0;
+    for (int i = 1; i <= N; ++i)
+    {
+        if((i & 1) == 0) cout << a << " ";
+        nextTerm = a + b;
+        a = b;
+        b = nextTerm;
+    }
+    return 0;
 }
