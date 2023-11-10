64cc1c9e7cb46644c00856cf575833fde2d5e4d4
pazhukov <k.a.pazukov@gmail.com>
2023-11-10 23:55:46 +0300

    add function

--- main.cpp
+++ main.cpp
@@ -1,21 +1,31 @@
 #include <iostream>
 using namespace std;
 
-int main()
+int fibonacci(int N)
 {
-    int N;
-    cout << "Введите количество итераций N: ";
-    cin >> N;
-
+    int ticker = 0;
     int a = 0, b = 1, nextTerm;
     cout << "Ряд чисел Фибоначчи до " << N << " итераций: ";
-
-    for (int i = 1; i <= N; ++i)
+    for (int i = 1; i <= N; i++)
     {
-        if((i & 1) == 0) cout << a << " ";
+        if (ticker == 3)
+        {
+            cout << a << " ";
+            ticker = 0;
+        }
         nextTerm = a + b;
         a = b;
         b = nextTerm;
+        ticker++;
     }
     return 0;
+}
+
+int main()
+{
+    int iteration = 0;
+    cout << "Введите количество итераций N: ";
+    cin >> iteration;
+    fibonacci(iteration);
+    cout << endl;
 }
